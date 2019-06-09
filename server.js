d081b7fe0263d28cc973f2fd2d097c971470b793
ndo3 <yo@ni.co.de>
2019-06-09 22:35:59 +0200

    completed jira api integration, migrated to .env file for config

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
-const moment = require('moment')
-const { DateTime } = require('luxon')
+const dotenv = require('dotenv')
 
 const app = express()
+dotenv.config()
 
 app.use(express.static(__dirname + '/'))
 app.use(helmet())
@@ -20,93 +20,53 @@ app.post('/jira', (req, res) => {
     
     var jira = new JiraApi({
     protocol: 'https',
-    host: 'task.syzygy.de',
-    username: 'nico.domino',
-    password: 'Miney91*',
+    host: process.env.JIRA_URL,
+    username: process.env.JIRA_USER,
+    password: process.env.JIRA_PW,
     apiVersion: '2',
     strictSSL: true
     })
 
-    const userName = 'nico.domino'
-    // let issueDesc = []
-    let issuePromises = []
-    let issueKeyResp = []
-
-    jiraGetIssue = (issueKeys) => {
-
-        issueKeyResp = issueKeys.forEach(key => {
-            let issueDetail = jira.findIssue(key)
-
-            issueDetail.then((resp) => {
-                return new Promise((resolve) => {
-                    console.log(resp.fields)
-                    issDesc = issue + ' - ' + resp.fields.summary
-                    // return issDesc
-                    // return res.fields.description
-                    console.log(JSON.stringify(issDesc))
-                    // const issResponse = JSON.stringify(issDesc)
-                    // return res.status(202).send(issResponse)
-
-                })
-            })
-            // .then(function(data) {
-            //     return Promise.all(issues2.map(data))
-            // })
-        })
-
+    issueDeetFn = (key) => {
+        let issueDetail = jira.findIssue(key)
+        return issueDetail
     }
 
-    // issueKeyResp.all (?)
-    let results2 = ''
-
-    jira.getUsersIssues(userName)
+    jira.getUsersIssues(process.env.JIRA_USER)
     .then(issue => {
-        // atlassian API response
-        // obj containing ALL issues info
         const issueObj = issue.issues
+        let outputObj = {}
+        let outputObj2 = {}
         let issueArr = []
 
-        // pushing all issue keys from the obj to an array of keys
         issueObj.forEach(iss => {
             issueArr.push(iss.key)
         })
 
-        // looping over issue key array to get
-        // the issue details based on issue id (key)
-        console.dir(issueArr)
-        jiraGetIssue(issueArr)
-        // for(i = 0; i < issueArr.length; i++) {
-        //     issuePromises.forEach(jiraGetIssue(issueArr[i]))
-        // }
-    })
-    .then(function(data){
-        return Promise.all(results2.map(data))
+        let omgzPromises = issueArr.map(issueDeetFn)
+        let omgzResults = Promise.all(omgzPromises)
+
+        omgzResults.then(data => {
+            for(i = 0; i < data.length; i++) {
+                const issueKey = data[i].key
+                const issueSummary = data[i].fields.summary
+
+                outputObj[issueKey] = issueSummary
+            }
+            outputObj2['issues'] = outputObj
+
+            const outputJSON = JSON.stringify(outputObj2)
+            return res.status(202).send(outputJSON)
+        })
     })
     .catch(err => {
         console.error(err)
         return res.status(403).send(err)
     })
 
-    let results = Promise.all(issuePromises) 
-    results.then(data => 
-        console.log(data)
-    )
-
-    console.log('r2: ' + results2)
-
-    console.log('iP', issuePromises)
-    Promise.all(issuePromises)
-        .then((result) => {
-            // console.log('result', result)
-            // console.log('issueDesc', issueDesc)
-            // const issueResp = JSON.stringify(issueDesc)
-            // console.log('issueResp', issueResp)
-            // return res.status(202).send(issueResp)
-        })
-
 })
 
-port = 7557
+port = process.env.PORT || 7557
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
 })
