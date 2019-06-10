a7e2c8e4536549a59e3ce88417a30a4d0bd3e3cb
ndo@ndo7 <yo@ni.co.de>
2019-06-10 18:58:47 +0200

    removed console.log from server.js

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.post('/jira', (req, res) => {
 
         omgzResults.then(data => {
             for(i = 0; i < data.length; i++) {
-                console.log(data[i])
                 const issueKey = data[i].key
                 const issueSummary = data[i].fields.summary
 
